cb9bacde19e99c5ce079989809ab57700bd6597d
Sunil Arora <arora.sunil@gmail.com>
2012-02-26 21:29:52 +0530

    refactored and cleaned up async_if file

--- async_if.coffee
+++ async_if.coffee
@@ -1,11 +1,4 @@
-root = exports ? this
-
-
-is_box_visible = (color)->
-	selector = ".#{color}box:visible"
-	return $(selector).length > 0
-
-watch = (fn, args, timeout)->
+async_if = (fn, args, timeout)->
 	dfd = new jQuery.Deferred()
 	curr_probe = null
 
@@ -25,59 +18,5 @@ watch = (fn, args, timeout)->
 	probe()
 	return dfd.promise()
 
-inject_box = (color)->
-	box_html = """<div id="#{color}-box" class='box #{color}box'></div"""
-	$(box_html).appendTo('#canvas')
-
-click_handler = (ev)->
-	$target = $(ev.target)
-	if $target.is('#red-box')
-		return if is_box_visible('red')
-		inject_box('red')
-	else if $target.is('#green-box')
-		return if is_box_visible('green')
-		inject_box('green')
-
-place_guider_note = ->
-					guider_note =
-						attachTo: "#blue-box"
-						buttons: [{name: "Close", onclick: guiders.hideAll}]
-						description:"""Voila!
-									This box appeared because both red and green boxes have appeared on
-									our screen. """
-						position: 2
-						title: "Blue Box"
-						width: 230
-					guiders.createGuider(guider_note).show()
-
-place_intro_guider_note = ->
-					guider_note =
-						attachTo: "#nav"
-						buttons: [{name: "Close", onclick: guiders.hideAll}]
-						description:"""
-									1. Use buttons to inject color boxes below <br/>
-									2. If green and red boxes appear on the screen, blue box will appear automatically<br />
-									"""
-						position: 2
-						title: "Inject Boxes"
-						width: 230
-					guiders.createGuider(guider_note).show()
-
-initialize = ()->
-	$('#nav').bind('click', click_handler)
-	place_intro_guider_note()
-	watch(is_box_visible, 'red').then(
-		(status)->
-		,
-		(status)->
-			alert "#{status} with failure ...."
-		)
-
-	$.when(watch(is_box_visible, 'green'),
-			watch(is_box_visible, 'red')).then(()->
-												inject_box('blue')
-												place_guider_note()
-											)
-	
-
-$(document).ready initialize
+root = exports ? this
+root.async_if = async_if
--- async_if.js
+++ async_if.js
@@ -1,15 +1,7 @@
 (function() {
-  var click_handler, initialize, inject_box, is_box_visible, place_guider_note, place_intro_guider_note, root, watch;
+  var async_if, root;
 
-  root = typeof exports !== "undefined" && exports !== null ? exports : this;
-
-  is_box_visible = function(color) {
-    var selector;
-    selector = "." + color + "box:visible";
-    return $(selector).length > 0;
-  };
-
-  watch = function(fn, args, timeout) {
+  async_if = function(fn, args, timeout) {
     var curr_probe, dfd, probe;
     dfd = new jQuery.Deferred();
     curr_probe = null;
@@ -31,72 +23,8 @@
     return dfd.promise();
   };
 
-  inject_box = function(color) {
-    var box_html;
-    box_html = "<div id=\"" + color + "-box\" class='box " + color + "box'></div";
-    return $(box_html).appendTo('#canvas');
-  };
+  root = typeof exports !== "undefined" && exports !== null ? exports : this;
 
-  click_handler = function(ev) {
-    var $target;
-    $target = $(ev.target);
-    if ($target.is('#red-box')) {
-      if (is_box_visible('red')) return;
-      return inject_box('red');
-    } else if ($target.is('#green-box')) {
-      if (is_box_visible('green')) return;
-      return inject_box('green');
-    }
-  };
-
-  place_guider_note = function() {
-    var guider_note;
-    guider_note = {
-      attachTo: "#blue-box",
-      buttons: [
-        {
-          name: "Close",
-          onclick: guiders.hideAll
-        }
-      ],
-      description: "Voila!\nThis box appeared because both red and green boxes have appeared on\nour screen. ",
-      position: 2,
-      title: "Blue Box",
-      width: 230
-    };
-    return guiders.createGuider(guider_note).show();
-  };
-
-  place_intro_guider_note = function() {
-    var guider_note;
-    guider_note = {
-      attachTo: "#nav",
-      buttons: [
-        {
-          name: "Close",
-          onclick: guiders.hideAll
-        }
-      ],
-      description: "1. Use buttons to inject color boxes below <br/>\n2. If green and red boxes appear on the screen, blue box will appear automatically<br />",
-      position: 2,
-      title: "Inject Boxes",
-      width: 230
-    };
-    return guiders.createGuider(guider_note).show();
-  };
-
-  initialize = function() {
-    $('#nav').bind('click', click_handler);
-    place_intro_guider_note();
-    watch(is_box_visible, 'red').then(function(status) {}, function(status) {
-      return alert("" + status + " with failure ....");
-    });
-    return $.when(watch(is_box_visible, 'green'), watch(is_box_visible, 'red')).then(function() {
-      inject_box('blue');
-      return place_guider_note();
-    });
-  };
-
-  $(document).ready(initialize);
+  root.async_if = async_if;
 
 }).call(this);
